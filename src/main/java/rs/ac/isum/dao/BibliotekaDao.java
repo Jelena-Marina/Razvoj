8be19b3621f906c71aced991911d9811674cce92
lapatop <lapatop@192.168.88.214>
2014-05-23 14:39:48 +0200

    Merge origin/master

--- src/main/java/rs/ac/isum/dao/BibliotekaDao.java
+++ src/main/java/rs/ac/isum/dao/BibliotekaDao.java
@@ -7,9 +7,17 @@
 package rs.ac.isum.dao;
 
 import java.util.List;
-import rs.ac.isum.entities.AbstractEntity;
 import rs.ac.isum.entities.Autor;
+import rs.ac.isum.entities.Izdavac;
 import rs.ac.isum.entities.Jezik;
+import rs.ac.isum.entities.Knjiga;
+import rs.ac.isum.entities.Oblast;
+import rs.ac.isum.entities.Osoba;
+import rs.ac.isum.entities.Publikacija;
+import rs.ac.isum.entities.Student;
+import rs.ac.isum.entities.Zaduzenja;
+import rs.ac.isum.entities.Zaposleni;
+import rs.ac.isum.entities.ZaposleniBibliotekar;
 
 /**
  *
@@ -19,5 +27,24 @@ public interface BibliotekaDao {
     
     public abstract List<Autor> sviAutori();
     public abstract void obrisiAutora();
+    
+    public abstract List<Izdavac> sviIzdavaci();
+    
     public abstract List<Jezik> sviJezici();
+    
+    public abstract List<Knjiga> sveKnjige();
+    
+    public abstract List<Oblast> sveOblaste();
+    
+    public abstract List<Osoba> sveOsobe();
+    
+    public abstract List<Publikacija> svePublikacija();
+    
+    public abstract List<Student> sviStudenti();
+    
+    public abstract List<Zaduzenja> svaZaduzenja();
+    
+    public abstract List<Zaposleni> sviZaposleni();
+    
+    public abstract List<ZaposleniBibliotekar> sviBibliotekari();
 }
--- src/main/java/rs/ac/isum/dao/BibliotekaDaoImpl.java
+++ src/main/java/rs/ac/isum/dao/BibliotekaDaoImpl.java
@@ -10,7 +10,16 @@ import java.util.List;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.hibernate.Session;
 import rs.ac.isum.entities.Autor;
+import rs.ac.isum.entities.Izdavac;
 import rs.ac.isum.entities.Jezik;
+import rs.ac.isum.entities.Knjiga;
+import rs.ac.isum.entities.Oblast;
+import rs.ac.isum.entities.Osoba;
+import rs.ac.isum.entities.Publikacija;
+import rs.ac.isum.entities.Student;
+import rs.ac.isum.entities.Zaduzenja;
+import rs.ac.isum.entities.Zaposleni;
+import rs.ac.isum.entities.ZaposleniBibliotekar;
 
 /**
  *
@@ -36,6 +45,51 @@ public class BibliotekaDaoImpl implements BibliotekaDao{
 
     @Override
     public void obrisiAutora() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Izdavac> sviIzdavaci() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Knjiga> sveKnjige() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Oblast> sveOblaste() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Osoba> sveOsobe() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Publikacija> svePublikacija() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Student> sviStudenti() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Zaduzenja> svaZaduzenja() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Zaposleni> sviZaposleni() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<ZaposleniBibliotekar> sviBibliotekari() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     

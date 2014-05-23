17392ee9f44750a450e410bdfdee34ce269ed86a
Jelena-Marina Bradaric <Jelena@Jelena-NB>
2014-05-23 16:11:05 +0200

    Nov AE

--- src/main/java/rs/ac/isum/entities/AbstractEntity.java
+++ src/main/java/rs/ac/isum/entities/AbstractEntity.java
@@ -7,16 +7,13 @@
 package rs.ac.isum.entities;
 
 import java.io.Serializable;
-import java.lang.Long;
 import java.util.Date;
 import javax.persistence.Basic;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -44,28 +41,6 @@ public class AbstractEntity implements Serializable {
     private Date lastModificationTime;
     @Column(name = "LAST_MODIFIED_BY_USER")
     private String lastModifiedByUser;
-    @OneToOne(cascade = CascadeType.ALL, mappedBy = "abstractEntity")
-    private Knjiga knjiga;
-    @OneToOne(cascade = CascadeType.ALL, mappedBy = "abstractEntity")
-    private Autor autor;
-    @OneToOne(cascade = CascadeType.ALL, mappedBy = "abstractEntity")
-    private Oblast oblast;
-    @OneToOne(cascade = CascadeType.ALL, mappedBy = "abstractEntity")
-    private Jezik jezik;
-    @OneToOne(cascade = CascadeType.ALL, mappedBy = "abstractEntity")
-    private Student student;
-    @OneToOne(cascade = CascadeType.ALL, mappedBy = "abstractEntity")
-    private Zaduzenja zaduzenja;
-    @OneToOne(cascade = CascadeType.ALL, mappedBy = "abstractEntity")
-    private Osoba osoba;
-    @OneToOne(cascade = CascadeType.ALL, mappedBy = "abstractEntity")
-    private AutoriLista autoriLista;
-    @OneToOne(cascade = CascadeType.ALL, mappedBy = "abstractEntity")
-    private Publikacija publikacija;
-    @OneToOne(cascade = CascadeType.ALL, mappedBy = "abstractEntity")
-    private Zaposleni zaposleni;
-    @OneToOne(cascade = CascadeType.ALL, mappedBy = "abstractEntity")
-    private Izdavac izdavac;
 
     public AbstractEntity() {
     }
@@ -109,94 +84,6 @@ public class AbstractEntity implements Serializable {
 
     public void setLastModifiedByUser(String lastModifiedByUser) {
         this.lastModifiedByUser = lastModifiedByUser;
-    }
-
-    public Knjiga getKnjiga() {
-        return knjiga;
-    }
-
-    public void setKnjiga(Knjiga knjiga) {
-        this.knjiga = knjiga;
-    }
-
-    public Autor getAutor() {
-        return autor;
-    }
-
-    public void setAutor(Autor autor) {
-        this.autor = autor;
-    }
-
-    public Oblast getOblast() {
-        return oblast;
-    }
-
-    public void setOblast(Oblast oblast) {
-        this.oblast = oblast;
-    }
-
-    public Jezik getJezik() {
-        return jezik;
-    }
-
-    public void setJezik(Jezik jezik) {
-        this.jezik = jezik;
-    }
-
-    public Student getStudent() {
-        return student;
-    }
-
-    public void setStudent(Student student) {
-        this.student = student;
-    }
-
-    public Zaduzenja getZaduzenja() {
-        return zaduzenja;
-    }
-
-    public void setZaduzenja(Zaduzenja zaduzenja) {
-        this.zaduzenja = zaduzenja;
-    }
-
-    public Osoba getOsoba() {
-        return osoba;
-    }
-
-    public void setOsoba(Osoba osoba) {
-        this.osoba = osoba;
-    }
-
-    public AutoriLista getAutoriLista() {
-        return autoriLista;
-    }
-
-    public void setAutoriLista(AutoriLista autoriLista) {
-        this.autoriLista = autoriLista;
-    }
-
-    public Publikacija getPublikacija() {
-        return publikacija;
-    }
-
-    public void setPublikacija(Publikacija publikacija) {
-        this.publikacija = publikacija;
-    }
-
-    public Zaposleni getZaposleni() {
-        return zaposleni;
-    }
-
-    public void setZaposleni(Zaposleni zaposleni) {
-        this.zaposleni = zaposleni;
-    }
-
-    public Izdavac getIzdavac() {
-        return izdavac;
-    }
-
-    public void setIzdavac(Izdavac izdavac) {
-        this.izdavac = izdavac;
     }
 
     @Override
